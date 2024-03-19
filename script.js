925d0b2901f7e5c996fc997523cd41f6dc56bf81
Giovanni <gio.reder@gmail.com>
2024-03-19 15:25:38 +0100

    1.1

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-setTimeout(() => {
-  const h1 = document.querySelector("h1");
-  h1.innerText = "Timer scaduto";
-}, 3000);
-
 let counter = 60;
-
 const timer = setInterval(() => {
   counter--;
   const p = document.getElementById("seconds");
   p.innerText = counter;
+
+  const timerElement = document.getElementById("timer");
+  const borderPercentage = (counter / 60) * 100;
+  timerElement.style.border = `10px solid #00FFFF`;
 
   if (counter === 0) {
     clearInterval(timer);
